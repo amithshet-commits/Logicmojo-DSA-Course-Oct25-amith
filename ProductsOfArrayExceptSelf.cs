1b863fbc02aba813c55ee42c8af1d4cf382ebd5a
amithshet-commits <shetamiteee@gmail.com>
2025-10-19 16:52:02 +0530

    19th oct commit: Products of Array Except Self using prefix and suffix technique

---
+++ ProductsOfArrayExceptSelf.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int prod = 1; int[] res = new int[nums.Length];
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            res[i] = prod;
+            prod *= nums[i];
+        }
+        prod = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            res[i] *= prod;
+            prod *= nums[i];
+        }
+        return res;
+    }
+}
