c857ad78dda22852a1c6f14183aade0bd171fd3f
amithshet-commits <shetamiteee@gmail.com>
2025-11-06 00:38:16 +0530

    5th nov commit: Array problems.

---
+++ Arrays Problems/Hashings/LongestCommonPrefix.cs
@@ -0,0 +1,41 @@
+﻿//TC O(N*M) SC O(1)
+public string LongestCommonPrefix(string[] strs)
+{
+    string queryStr = strs[0];
+    string result = "";
+    int minLength = strs.Min(s => s.Length);
+    for (int i = 0; i < minLength; i++)
+    {
+        bool flag = true;
+        for (int j = 1; j < strs.Length; j++)
+        {
+            if (strs[j][i] != queryStr[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag)
+            break;
+        result += queryStr[i];
+    }
+    return result;
+}
+//TC O(N log N) SC O(1)
+public string LongestCommonPrefix(string[] strs)
+{
+    Array.Sort(strs);
+    string result = "";
+    for (int i = 0; i < strs[0].Length; i++)
+    {
+        if (strs[0][i] != strs[strs.Length - 1][i])
+        {
+            return result;
+        }
+        result += strs[0][i];
+    }
+    return result;
+}
+string[] strs = new string[] { "flower", "flow", "flight" };
+string result = LongestCommonPrefix(strs);
+Console.ReadLine();
---
+++ Arrays Problems/Hashings/LongestConsecutiveSequence.cs
@@ -0,0 +1,50 @@
+﻿//TC O(n) SC O(n)
+static int LongestConsecutive(int[] nums)
+{
+    if (nums.Length == 0)
+        return 0;
+    HashSet<int> set = new HashSet<int>(nums);
+    int max = 0;
+    foreach (int num in set)
+    {
+        // only start counting if this number is the START of a sequence
+        if (!set.Contains(num - 1))
+        {
+            int curr = num;
+            int len = 1;
+            // expand forward
+            while (set.Contains(curr + 1))
+            {
+                curr++;
+                len++;
+            }
+            max = Math.Max(max, len);
+        }
+    }
+    return max;
+}
+//TC O(nlogn) SC O(1)
+static int LongestConsecutive(int[] nums)
+{
+    if (nums.Length == 0)
+        return 0;
+    int max = 1; int currCount = 1;
+    Array.Sort(nums);
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        if (nums[i] == nums[i - 1] + 1)
+        {
+            currCount++;
+        }
+        else if (nums[i] != nums[i - 1] + 1)
+        {
+            currCount = 1;
+        }
+        max = Math.Max(max, currCount);
+    }
+    return max;
+}
