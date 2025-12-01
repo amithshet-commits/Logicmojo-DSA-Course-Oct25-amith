1678b10f38ade0948116410596012fa9dda1550d
amithshet-commits <shetamiteee@gmail.com>
2025-12-01 20:06:24 +0530

    1st Dec: Climbing stairs by recursion

---
+++ Recursion/ClimbingStairs.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        return Helper(0, n);
+    }
+
+    public int Helper(int i, int n)
+    {
+        if (i > n)
+        {
+            return 0;
+        }
+        if (i == n)
+        {
+            return 1;
+        }
+        int oneStep = Helper(i + 1, n);
+        int twoStep = Helper(i + 2, n);
+        return oneStep + twoStep;
+    }
+}
