43d9726ce529500a20d6e8af4648bfe82b9e2f28
amithshet-commits <shetamiteee@gmail.com>
2025-12-01 20:15:36 +0530

    1st Dec commit: UniquePaths by recursion.

---
+++ Recursion/UniquePaths.cs
@@ -0,0 +1,18 @@
+﻿static int UniquePaths(int m, int n)
+{
+    return helperUniquePaths(0, 0, m, n);
+}
+static int helperUniquePaths(int r, int d, int m, int n)
+{
+    if (r == m - 1 || d == n - 1)
+    {
+        return 1;
+    }
+    if (r >= m || d >= n)
+    {
+        return 0;
+    }
+    int right = helperUniquePaths(r + 1, d, m, n);
+    int down = helperUniquePaths(r, d + 1, m, n);
+    return right + down;
+}
