a3c799f5647637dbd2e1d550ab174f260e170cb5
amithshet-commits <shetamiteee@gmail.com>
2025-12-09 23:04:28 +0530

    9th dec commit: invert binary tree and max depth tree problems

---
+++ Trees/invertBinaryTree.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
---
+++ Trees/maxDepth.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        // Base Condition
+        if (root == null) return 0;
+        int left = MaxDepth(root.left);
+        int right = MaxDepth(root.right);
+        return Math.Max(left, right) + 1;
+    }
+}
