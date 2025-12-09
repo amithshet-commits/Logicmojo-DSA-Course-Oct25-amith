62b3bbf48497d4231f7f50107c337a33f3e98ebe
amithshet-commits <shetamiteee@gmail.com>
2025-12-09 22:53:30 +0530

    9th dec commit: Post order traversal tree problem.

---
+++ Trees/PreOrderTraversal.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    List<int> res = new List<int>();
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        if (root == null) return new List<int>();
+        List<int> res = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.Push(root);
+        while (stack.Count > 0)
+        {
+            TreeNode curr = stack.Pop();
+            res.Add(curr.val);
+            if (curr.right != null)
+            {
+                stack.Push(curr.right);
+            }
+            if (curr.left != null)
+            {
+                stack.Push(curr.left);
+            }
+        }
+        return res;
+    }
+}
