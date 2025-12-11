ac2d173ddf5f111cfc07e219dfe102f807bc5440
amithshet-commits <shetamiteee@gmail.com>
2025-12-12 00:51:16 +0530

    12th dec commit: height of binary tree.

---
+++ Trees/height-of-binary-tree.cs
@@ -0,0 +1,11 @@
+﻿public static int getHeight(Node root)
+{
+    if (root == null)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1 + Math.Max(getHeight(root.left), getHeight(root.right));
+    }
+}
