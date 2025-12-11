c4fc456c4ee5d73bc52395e2332a4d88536c8f79
amithshet-commits <shetamiteee@gmail.com>
2025-12-12 01:08:18 +0530

    12th dec commit: validate  binary search tree

---
+++ Trees/validateBinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        return valid(root, long.MinValue, long.MaxValue);
+    }
+    private bool valid(TreeNode root, long minValue, long maxValue)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+        if (!(root.val > minValue && root.val < maxValue))
+        {
+            return false;
+        }
+        return valid(root.left, minValue, root.val) && valid(root.right, root.val, maxValue);
+    }
+}
