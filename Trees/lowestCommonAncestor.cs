692444e115bd1d3986ff863f99bc79b6b24441a0
amithshet-commits <shetamiteee@gmail.com>
2025-12-13 18:46:28 +0530

    13th dec commit: LowestCommonAncestor tree problem

---
+++ Trees/lowestCommonAncestor.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null || root == p || root == q)
+        {
+            return root;
+        }
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+        {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
