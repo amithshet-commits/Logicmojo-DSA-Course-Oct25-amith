3fad591fbd1b6f852494a4f21663993ab6b330d2
amithshet-commits <shetamiteee@gmail.com>
2025-12-13 08:42:37 +0530

    13th dec commit: Lowest Common Ancestor Of Binary Search Tree

---
+++ Trees/lowestCommonAncestorOfBinarySearchTree.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        while (root != null)
+        {
+            if (p.val > root.val && q.val > root.val)
+            {
+                root = root.right;
+            }
+            else if (p.val < root.val && q.val < root.val)
+            {
+                root = root.left;
+            }
+            else
+            {
+                return root;
+            }
+        }
+        return null;
+    }
+}
