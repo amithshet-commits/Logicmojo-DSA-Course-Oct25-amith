94ff3583b866e5963984a2c533108d2868d8dd7e
amithshet-commits <shetamiteee@gmail.com>
2025-12-21 19:58:34 +0530

    21st dec commit: Binary Tree Maximum Path Sum

---
+++ Trees/binary-tree-maximum-path-sum.cs
@@ -0,0 +1,22 @@
+﻿public class solution
+{
+    int maxSum = 0;
+    public int MaxPathSum(TreeNode root)
+    {
+        return maxSum;
+    }
+    public int MaxGain(TreeNode root)
+    {
+        if (root==null)
+        {
+            return 0;
+        }
+        int leftGain = Math.Max(MaxGain(root.left, 0));
+        int rightGain = Math.Max(MaxGain(root.right, 0));
+
+        int currMaxSum=root.val+leftGain + rightGain;
+        maxSum = Math.Max(currMaxSum,maxSum);
+
+        return root.val + Math.Max(leftGain, rightGain);
+    }
+}
