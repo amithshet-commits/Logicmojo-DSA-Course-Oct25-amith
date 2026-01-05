1dc78fec12199d8a75abfa5e5d8024e86452540a
amithshet-commits <shetamiteee@gmail.com>
2026-01-06 00:48:48 +0530

    6th jan commit: clone graph.

---
+++ Graphs/cloneGraph.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    private Dictionary<Node, Node> copies = new Dictionary<Node, Node>();
+
+    public Node CloneGraph(Node node)
+    {
+        if (node == null)
+            return null;
+
+        Node copy = new Node(node.val, new List<Node>());
+        copies[node] = copy;
+
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(node);
+
+        while (queue.Count > 0)
+        {
+            Node curr = queue.Dequeue();
+
+            foreach (Node neighbor in curr.neighbors)
+            {
+                if (!copies.ContainsKey(neighbor))
+                {
+                    copies[neighbor] = new Node(neighbor.val, new List<Node>());
+                    queue.Enqueue(neighbor);
+                }
+
+                copies[curr].neighbors.Add(copies[neighbor]);
+            }
+        }
+
+        return copy;
+    }
+}
