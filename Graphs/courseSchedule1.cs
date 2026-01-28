e39ff96b6a2a11122d5303904723c712446cbc37
amithshet-commits <shetamiteee@gmail.com>
2026-01-28 08:39:59 +0530

    28th jan commit, course schedule 1 and 2.

---
+++ Graphs/courseSchedule1.cs
@@ -0,0 +1,83 @@
+﻿public class Solution
+{
+    //brute force approach
+    //public bool CanFinish(int numCourses, int[][] prerequisites)
+    //{
+    //    List<List<int>> adj = new List<List<int>>();
+
+    //    for (int i = 0; i < numCourses; i++)
+    //        adj.Add(new List<int>());
+
+    //    foreach (var p in prerequisites)
+    //        adj[p[1]].Add(p[0]);
+
+    //    for (int i = 0; i < numCourses; i++)
+    //    {
+    //        bool[] path = new bool[numCourses];
+    //        if (HasCycle(i, adj, path))
+    //            return false;
+    //    }
+
+    //    return true;
+    //}
+    //public bool HasCycle(int course, List<List<int>> adj, bool[] path)
+    //{
+    //    if (path[course])
+    //        return true;
+
+    //    path[course] = true;
+
+    //    foreach (var neighbor in adj[course])
+    //    {
+    //        if (HasCycle(neighbor, adj, path))
+    //            return true;
+    //    }
+    //    path[course] = false;
+    //    return false;
+    //}
+
+    //topological sort or Kahn's algorithm 
+    static bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        List<List<int>> adj = new List<List<int>>();
+        int[] indegree = new int[numCourses];
+        List<int> ans = new List<int>();
+
+        for (int i = 0; i < numCourses; i++)
+            adj.Add(new List<int>());
+
+        foreach (var pair in prerequisites)
+        {
+            int course = pair[0];
+            int coursePrerequisite = pair[1];
+
+            adj[coursePrerequisite].Add(course);
+            indegree[course]++;
+        }
+
+        Queue<int> queue = new Queue<int>();
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (indegree[i] == 0)
+                queue.Enqueue(i);
+        }
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            ans.Add(current);
+
+            if (adj[current] != null)
+            {
+                foreach (var neighbor in adj[current])
+                {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0)
+                        queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        return ans.Count == numCourses;
+    }
+}
---
+++ Graphs/courseSchedule2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class GfG
+{
+
+    static int[] CanFinish(int numCourses, int[][] prerequisites)
+    {
+        List<List<int>> adj = new List<List<int>>();
+        List<int> ans = new List<int>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+            adj.Add(new List<int>());
+
+        foreach (var pair in prerequisites)
+        {
+            int course = pair[0];
+            int preCourse = pair[1];
+
+            adj[preCourse].Add(course);
+            indegree[course]++;
+        }
+
+        Queue<int> queue = new Queue<int>();
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (indegree[i] == 0)
+                queue.Enqueue(i);
+        }
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            ans.Add(current);
+
+            if (adj[current] != null)
+            {
+                foreach (var neighbor in adj[current])
+                {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0)
+                        queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        if (ans.Count != numCourses)
+            return new int[0];
+
+        return ans.ToArray();
+    }
+
+
+    static void Main()
+    {
+        int[][] prerequisites = new int[][]
+{
+    new int[] {1, 0},
+    new int[] {1, 2},
+    new int[] {0, 1}
+};
+        int[] result = CanFinish(3, prerequisites);
+        Console.ReadLine();
+    }
+}
