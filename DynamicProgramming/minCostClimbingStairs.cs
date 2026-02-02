955d9fd11dc6fe27aa0905c04b4f96fb5b94c743
amithshet-commits <shetamiteee@gmail.com>
2026-02-02 23:16:25 +0530

    2nd feb commit: min cost climbing stairs by memoization and tabulization

---
+++ DynamicProgramming/minCostClimbingStairs.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        // int[] memo=new int[cost.Length];
+        // for(int i=0;i<memo.Length;i++)
+        //     memo[i]=-1;
+        // return Math.Min(dfs(cost,0,memo),dfs(cost,1,memo));
+        for (int i = cost.Length - 3; i >= 0; i--)
+        {
+            cost[i] += Math.Min(cost[i + 1], cost[i + 2]);
+        }
+        return Math.Min(cost[0], cost[1]);
+    }
+
+    private int dfs(int[] cost, int i, int[] memo)
+    {
+        if (i >= cost.Length)
+            return 0;
+        if (memo[i] != -1)
+            return memo[i];
+        memo[i] = cost[i] + Math.Min(dfs(cost, i + 1, memo), dfs(cost, i + 2, memo));
+        return memo[i];
+    }
+}
